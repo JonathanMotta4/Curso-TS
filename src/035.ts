c54a003790636278b033bd7cfe448cb9fbb474dc
Jonathan Motta <jonathan.motta44@gmail.com>
2023-10-17 18:24:19 -0300

    Aula 35 concluida

---
+++ src/035.ts
@@ -0,0 +1,19 @@
+let [nn1,nn2,nn3,nn4]=['Aaron Manning','Juan Wagner','Ernest Ramsey','Jordan Dennis']
+
+const obj={
+    nome1:'Zachary Clarke',
+    nome2:'Josephine Lambert',
+    nome3:'Justin Matthews',
+    nome4:'Edith Hines',
+    nome5:'Susie Andrews',
+    
+}
+
+let {nome1,nome2,nome3,nome4,nome5} =obj
+
+console.log(nome1);
+console.log(nome2);
+console.log(nome3);
+console.log(nome4);
+console.log(nome5);
+
